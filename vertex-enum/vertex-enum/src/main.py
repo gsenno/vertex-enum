e2fed5139980d812e1f06057459e7cfa1a77716f
gsenno <gsenno@PC0867.ad.icfo.net>
2019-01-10 10:03:50 +0100

    escribir los funcionales en un archivo

--- vertex-enum/vertex-enum/src/main.py
+++ vertex-enum/vertex-enum/src/main.py
@@ -1,18 +1,25 @@
 import numpy as np
 from bellpolytope import BellPolytope
+  
 
-
-    
 if __name__ == '__main__':
- 
+
+
     poly = BellPolytope(3,2)    
+
     ineffResistInequalities = poly.getInefficiencyResistantInequalities()
-    
     vertices33 = BellPolytope(3,3).getVertices();
-    relevantIneqs = filter(lambda ineq : min(
-        map(lambda vertex : np.dot(ineq[1:],vertex),vertices33))<=-2,
-        ineffResistInequalities)
-    
-    print(len(relevantIneqs))
-    print(relevantIneqs[11][1:])
+
+    relevantIneqs = list(filter(lambda ineq : min(
+        map(lambda vertex : np.dot(ineq[1:],vertex),vertices33))<-1,
+        ineffResistInequalities))
+   
+
+    Ineq=np.zeros((20,81))
+    for i in range (0,20):
+        for j in range (0,81):
+            Ineq[i][j]=relevantIneqs[i][1+j]
+    with open('Ineqs.txt','wb') as f:
+            np.savetxt(f, Ineq, fmt='%.2f')
+
     
