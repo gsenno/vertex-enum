86a365ccbe0b309a6fea1d016a1b32a0b9566c3b
rravell <rravell@pcl0822>
2019-01-17 12:58:24 +0100

    Relabeling version 1 (not finished yet)

--- vertex-enum/vertex-enum/src/main.py
+++ vertex-enum/vertex-enum/src/main.py
@@ -2,6 +2,11 @@ import numpy as np
 from bellpolytope import BellPolytope
   
 
+def Alg(functional[i][j],j,H[j-1]):
+    
+    return H.tolist()
+
+
 if __name__ == '__main__':
 
     N=3
@@ -17,10 +22,35 @@ if __name__ == '__main__':
    
     numberOfCoefficients=N**2*(K+1)**2
     Ineq=np.zeros((len(relevantIneqs),numberOfCoefficients))
+    
     for i in range (0,len(relevantIneqs)):
         for j in range (0,numberOfCoefficients):
             Ineq[i][j]=relevantIneqs[i][1+j]
-    with open('Ineqs.txt','wb') as f:
-            np.savetxt(f, Ineq, fmt='%.2f')
+    functional=np.zeros((len(relevantIneqs),numberOfCoefficients))
+    functional[i]=Ineq[i][:]
+    
+    with open('Ineqs.txt','w') as f:
+                f.write('Minimal representative functional \n')
+                
+    minimalfunctionals =[]
+    permutedfunctional=[]
+    for i in range (0,len(relevantIneqs)):
+        H=[[] for x in xrange(numberOfCoefficients)]
+        for k in range (1, numberOfCoefficients):
+            H.append(k) #IDENTITY    
+
+        for j in range (1, numberOfCoefficients+1):
+            H=Alg(functional[i][:], j, H)
+    
+        for l in range (0, len(H)):
+            for s in range (0, numberOfCoefficients):
+                permutedfunctional[s]=functional[i][H[l][s]]
+            minimalfunctionals.append(permutedfunctional)
+    
+    output = set()
+    for x in minimalfunctionals:
+        output.add(x)
+    print output
+    
 
     
